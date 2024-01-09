694276ce9782a365f4632415fdfc09477ec945df
1243557 <steli3117@gmail.com>
2024-01-09 23:00:34 +0200

    got backpack working + added basic producer crud

---
+++ app/Http/Requests/GenreRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GenreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3|max:20',
+            'description' => 'required|min:5|max:30'
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.min' => "The genre's name cannot be shorter then 3 symbols",
+            'name.max' => "The genre's name is too long (20 characters limit)."
+        ];
+    }
+}
